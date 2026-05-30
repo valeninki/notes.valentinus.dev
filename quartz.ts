f8c7d291f4a1afc26dee04d6b20f9dd0129d7f52
valeninki <valeninki@users.noreply.github.com>
2026-05-30 12:23:47 +0300

    remove topics from recent notes and explorer

--- quartz.ts
+++ quartz.ts
@@ -2,8 +2,12 @@ import type { QuartzPluginData } from "@quartz-community/types"
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
 import * as ExternalPlugin from "./.quartz/plugins"
 
+ExternalPlugin.Explorer({
+  filterFn: (node: { slugSegment?: string }) => node.slugSegment !== "topics",
+})
+
 ExternalPlugin.RecentNotes({
-  filter: (f: QuartzPluginData & Record<string, unknown>) => f.slug !== "Topics" && f.slug !== "index",
+  filter: (f: QuartzPluginData & Record<string, unknown>) => f.slug !== "topics" && f.slug !== "index",
 })
 
 const config = await loadQuartzConfig()
